c55423235ac47d36a11f151034d1ee5c82955a0b
Lyuben Dimitrov <ldimitrov070@gmail.com>
2014-03-07 19:13:49 +0100

    organized imports

--- src/com/ldimitrov/burnmessenger/MainActivity.java
+++ src/com/ldimitrov/burnmessenger/MainActivity.java
@@ -1,15 +1,11 @@
 package com.ldimitrov.burnmessenger;
 
-import java.util.Locale;
-
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
