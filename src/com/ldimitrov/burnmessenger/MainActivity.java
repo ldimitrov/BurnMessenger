e4c0b3fa1ff4c1cccc3e3971432d3b2309f1b32c
Lyuben Dimitrov <ldimitrov070@gmail.com>
2014-03-07 19:13:06 +0100

    extracted SectionsPagerAdapter to a new class

--- src/com/ldimitrov/burnmessenger/MainActivity.java
+++ src/com/ldimitrov/burnmessenger/MainActivity.java
@@ -63,8 +63,7 @@ public class MainActivity extends FragmentActivity implements
 
 		// Create the adapter that will return a fragment for each of the three
 		// primary sections of the app.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(
-				getSupportFragmentManager());
+		mSectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
 
 		// Set up the ViewPager with the sections adapter.
 		mViewPager = (ViewPager) findViewById(R.id.pager);
@@ -137,49 +136,7 @@ public class MainActivity extends FragmentActivity implements
 			FragmentTransaction fragmentTransaction) {
 	}
 
-	/**
-	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-	 * one of the sections/tabs/pages.
-	 */
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-		public SectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a DummySectionFragment (defined as a static inner class
-			// below) with the page number as its lone argument.
-			Fragment fragment = new DummySectionFragment();
-			Bundle args = new Bundle();
-			args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
-			fragment.setArguments(args);
-			return fragment;
-		}
-
-		@Override
-		public int getCount() {
-			// Show 3 total pages.
-			return 3;
-		}
-
-		@Override
-		public CharSequence getPageTitle(int position) {
-			Locale l = Locale.getDefault();
-			switch (position) {
-			case 0:
-				return getString(R.string.title_section1).toUpperCase(l);
-			case 1:
-				return getString(R.string.title_section2).toUpperCase(l);
-			case 2:
-				return getString(R.string.title_section3).toUpperCase(l);
-			}
-			return null;
-		}
-	}
-
+	
 	/**
 	 * A dummy fragment representing a section of the app, but that simply
 	 * displays dummy text.
---
+++ src/com/ldimitrov/burnmessenger/SectionsPagerAdapter.java
@@ -0,0 +1,57 @@
+package com.ldimitrov.burnmessenger;
+
+import java.util.Locale;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.ldimitrov.burnmessenger.MainActivity.DummySectionFragment;
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+	
+	protected Context mContext;
+
+	public SectionsPagerAdapter(Context context, FragmentManager fm) {
+		super(fm);
+		mContext = context;
+	}
+
+	@Override
+	public Fragment getItem(int position) {
+		// getItem is called to instantiate the fragment for the given page.
+		// Return a DummySectionFragment (defined as a static inner class
+		// below) with the page number as its lone argument.
+		Fragment fragment = new DummySectionFragment();
+		Bundle args = new Bundle();
+		args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+		fragment.setArguments(args);
+		return fragment;
+	}
+
+	@Override
+	public int getCount() {
+		// Show 3 total pages.
+		return 3;
+	}
+
+	@Override
+	public CharSequence getPageTitle(int position) {
+		Locale l = Locale.getDefault();
+		switch (position) {
+		case 0:
+			return mContext.getString(R.string.title_section1).toUpperCase(l);
+		case 1:
+			return mContext.getString(R.string.title_section2).toUpperCase(l);
+		case 2:
+			return mContext.getString(R.string.title_section3).toUpperCase(l);
+		}
+		return null;
+	}
+}
