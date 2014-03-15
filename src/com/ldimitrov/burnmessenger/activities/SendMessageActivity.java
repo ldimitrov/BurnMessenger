f3b94409a104c546b75b4188f0d38492e6f7d0a8
Lyuben Dimitrov <ldimitrov070@gmail.com>
2014-03-15 17:53:27 +0100

    Added onClickListeners and passed message to Recipients

--- src/com/ldimitrov/burnmessenger/activities/SendMessageActivity.java
+++ src/com/ldimitrov/burnmessenger/activities/SendMessageActivity.java
@@ -1,20 +1,49 @@
 package com.ldimitrov.burnmessenger.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.ldimitrov.burnmessenger.util.ParseConstants;
 
 public class SendMessageActivity extends Activity {
 
     protected MenuItem mSendMenuItem;
+    protected Button mMessageButton;
+    protected EditText mMessage;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_message);
 
         setupActionBar();
+
+        mMessageButton = (Button)findViewById(R.id.send_button);
+        mMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMessage = (EditText)findViewById(R.id.edit_text_message);
+                String messageValue = mMessage.getText().toString();
+                if(TextUtils.isEmpty(messageValue)){
+                    Toast.makeText(SendMessageActivity.this, "Enter a message first!", Toast.LENGTH_LONG).show();
+                } else {
+                    Intent recipientsIntent = new Intent(SendMessageActivity.this, RecipientsActivity.class);
+                    recipientsIntent.putExtra(ParseConstants.KEY_MESSAGE, messageValue);
+                    recipientsIntent.putExtra(ParseConstants.KEY_FILE_TYPE, ParseConstants.TYPE_TEXT);
+                    startActivity(recipientsIntent);
+                    finish();
+                }
+            }
+        });
+
     }
 
     private void setupActionBar() {
@@ -40,8 +69,6 @@ public class SendMessageActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 }
-
-
     /*final Dialog messageDialog = new Dialog(this);
     messageDialog.setContentView(R.layout.message_bar);
     messageDialog.setTitle(R.string.send_message_title);
