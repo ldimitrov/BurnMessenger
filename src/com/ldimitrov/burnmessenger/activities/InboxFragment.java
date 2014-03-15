08bdfa5d037c9df86693d08477acd17cfca9c93b
Lyuben Dimitrov <ldimitrov070@gmail.com>
2014-03-15 01:42:01 +0100

    added queries to get messages matching recipientID in InboxFragment

--- src/com/ldimitrov/burnmessenger/activities/InboxFragment.java
+++ src/com/ldimitrov/burnmessenger/activities/InboxFragment.java
@@ -5,9 +5,21 @@ import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.ldimitrov.burnmessenger.util.ParseConstants;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 
 public class InboxFragment extends ListFragment {
+
+    protected List<ParseObject> mMessages;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -15,6 +27,43 @@ public class InboxFragment extends ListFragment {
                 container, false);
 
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getActivity().setProgressBarIndeterminate(true);
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(ParseConstants.CLASS_MESSAGES);
+        // all messages where current user ID is recipient
+        query.whereEqualTo(ParseConstants.KEY_RECIPIENT_IDS, ParseUser.getCurrentUser().getObjectId());
+        // make most recent messages appear on top of inbox
+        query.addAscendingOrder(ParseConstants.KEY_CREATED_AT);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> messages, ParseException e) {
+                getActivity().setProgressBarIndeterminate(false);
+                if(e == null) {
+                    // success - display messages
+                    mMessages = messages;
+
+                    String[] usernames = new String[mMessages.size()];
+                    int i = 0;
+                    for(ParseObject message : mMessages) {
+                        usernames[i] = message.getString(ParseConstants.KEY_SENDER_NAME);
+                        i++;
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                            getListView().getContext(),
+                            android.R.layout.simple_list_item_1,
+                            usernames);
+                    setListAdapter(adapter);
+
+
+                } else {
+
+                }
+            }
+        });
     }
 
 }
