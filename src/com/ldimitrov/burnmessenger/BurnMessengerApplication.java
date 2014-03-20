fbee0d07cafa11d4ce5ec429b9537ebab4946463
Lyuben Dimitrov <ldimitrov070@gmail.com>
2014-03-21 00:33:24 +0100

    added push notification infrastructure

--- src/com/ldimitrov/burnmessenger/BurnMessengerApplication.java
+++ src/com/ldimitrov/burnmessenger/BurnMessengerApplication.java
@@ -2,7 +2,9 @@ package com.ldimitrov.burnmessenger;
 
 import android.app.Application;
 
+import com.ldimitrov.burnmessenger.activities.MainActivity;
 import com.parse.Parse;
+import com.parse.PushService;
 
 public class BurnMessengerApplication extends Application{
 	
@@ -10,5 +12,6 @@ public class BurnMessengerApplication extends Application{
 	public void onCreate() {
 		super.onCreate();
 		Parse.initialize(this, "EgWS4wnqEX5hxmMTOEBp5axx9GcczodBR02CPORM", "nAXp60HEL4Suk3roA5tNmviuLihTCAyujGeXY5Sn");
+		PushService.setDefaultPushCallback(this, MainActivity.class);
 	}
 }
