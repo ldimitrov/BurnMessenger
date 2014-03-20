911bce52d040273e4fe7c2676d28aa5e8ee1b8c1
Lyuben Dimitrov <ldimitrov070@gmail.com>
2014-03-20 18:03:22 +0100

    fixed timestamps to relative

--- src/com/ldimitrov/burnmessenger/adapters/MessageAdapter.java
+++ src/com/ldimitrov/burnmessenger/adapters/MessageAdapter.java
@@ -1,10 +1,9 @@
 package com.ldimitrov.burnmessenger.adapters;
 
-import java.text.SimpleDateFormat;
 import java.util.List;
-import java.util.Locale;
 
 import android.content.Context;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,57 +17,68 @@ import com.parse.ParseObject;
 
 public class MessageAdapter extends ArrayAdapter<ParseObject> {
 
-    protected Context mContext;
-    protected List<ParseObject> mMessages;
+	protected Context mContext;
+	protected List<ParseObject> mMessages;
 
-    public MessageAdapter(Context context, List<ParseObject> messages) {
-        super(context, R.layout.message_item, messages);
-        mContext = context;
-        mMessages = messages;
-    }
+	public MessageAdapter(Context context, List<ParseObject> messages) {
+		super(context, R.layout.message_item, messages);
+		mContext = context;
+		mMessages = messages;
+	}
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder;
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder;
 
-        if (convertView == null) {
-            convertView = LayoutInflater.from(mContext).inflate(R.layout.message_item, parent, false);
-            holder = new ViewHolder();
-            holder.iconImageView = (ImageView) convertView.findViewById(R.id.messageIcon);
-            holder.nameLabel = (TextView) convertView.findViewById(R.id.senderLabel);
-            holder.sendTime = (TextView) convertView.findViewById(R.id.sendTime);
-            convertView.setTag(holder);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
-        }
+		if (convertView == null) {
+			convertView = LayoutInflater.from(mContext).inflate(
+					R.layout.message_item, parent, false);
+			holder = new ViewHolder();
+			holder.iconImageView = (ImageView) convertView
+					.findViewById(R.id.messageIcon);
+			holder.nameLabel = (TextView) convertView
+					.findViewById(R.id.senderLabel);
+			holder.sendTime = (TextView) convertView
+					.findViewById(R.id.sendTime);
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
 
-        ParseObject message = mMessages.get(position);
+		ParseObject message = mMessages.get(position);
 
-        if (message.getString(ParseConstants.KEY_FILE_TYPE).equals(ParseConstants.TYPE_IMAGE)) {
-            holder.iconImageView.setImageResource(R.drawable.ic_action_picture);
-        } else if (message.getString(ParseConstants.KEY_FILE_TYPE).equals(ParseConstants.TYPE_VIDEO)) {
-            holder.iconImageView.setImageResource(R.drawable.ic_action_play_over_video);
-        } else {
-            holder.iconImageView.setImageResource(R.drawable.ic_action_chat_light);
-        }
-        holder.nameLabel.setText(message.getString(ParseConstants.KEY_SENDER_NAME));
+		if (message.getString(ParseConstants.KEY_FILE_TYPE).equals(
+				ParseConstants.TYPE_IMAGE)) {
+			holder.iconImageView.setImageResource(R.drawable.ic_action_picture);
+		} else if (message.getString(ParseConstants.KEY_FILE_TYPE).equals(
+				ParseConstants.TYPE_VIDEO)) {
+			holder.iconImageView
+					.setImageResource(R.drawable.ic_action_play_over_video);
+		} else {
+			holder.iconImageView
+					.setImageResource(R.drawable.ic_action_chat_light);
+		}
+		holder.nameLabel.setText(message
+				.getString(ParseConstants.KEY_SENDER_NAME));
 
-        //CharSequence relativeTime = DateUtils.getRelativeDateTimeString(mContext, now.getTime(), DateUtils.MINUTE_IN_MILLIS, DateUtils.YEAR_IN_MILLIS, 0);
+		CharSequence relativeTime = DateUtils.getRelativeDateTimeString(
+				mContext, message.getCreatedAt().getTime(),
+				DateUtils.MINUTE_IN_MILLIS, DateUtils.YEAR_IN_MILLIS, 0);
 
-        holder.sendTime.setText(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US).format(message.getCreatedAt()));
+		holder.sendTime.setText(relativeTime);
 
-        return convertView;
-    }
+		return convertView;
+	}
 
-    public void refill(List<ParseObject> messages){
-        mMessages.clear();
-        mMessages.addAll(messages);
-        notifyDataSetChanged();
-    }
+	public void refill(List<ParseObject> messages) {
+		mMessages.clear();
+		mMessages.addAll(messages);
+		notifyDataSetChanged();
+	}
 
-    private static class ViewHolder {
-        ImageView iconImageView;
-        TextView nameLabel;
-        TextView sendTime;
-    }
+	private static class ViewHolder {
+		ImageView iconImageView;
+		TextView nameLabel;
+		TextView sendTime;
+	}
 }
