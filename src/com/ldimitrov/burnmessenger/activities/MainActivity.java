59fe5b8494ded6763a23169d0c62acef33d8afe5
Lyuben Dimitrov <ldimitrov070@gmail.com>
2014-05-15 22:42:29 +0200

    Updated Parse library

--- src/com/ldimitrov/burnmessenger/activities/MainActivity.java
+++ src/com/ldimitrov/burnmessenger/activities/MainActivity.java
@@ -329,4 +329,6 @@ public class MainActivity extends FragmentActivity implements
     @Override
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
     }
+
+
 }
